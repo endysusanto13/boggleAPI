8fe6a3988f29970673494b60ed7885976b5463d9
endysusanto13 <endy.susanto@gmail.com>
2021-06-14 17:37:27 +0800

    Update board.rb
    Simplify board.rb to use string instead of array for the board

--- app/board.rb
+++ app/board.rb
@@ -3,44 +3,35 @@ class Boggle
     @row, @col = 4, 4                                                                     # Number of rows and column of the board
     @total_letters = @row * @col
     
-    letter_string = check_board_type(unprocessed_string)
-    letter_string.length == @total_letters ? (@board = generate_board(letter_string)) : (puts "Invalid number of letters.")
+    letter_string = remove_characters_from_string(unprocessed_string)
+    generate_board(letter_string)
   end
   
   def remove_characters_from_string(string_with_characters)
-    letter_string = string_with_characters.tr(",", "").tr(" ", "").tr("\n", "").upcase    # Convert default board string to an array. Single quote ("") cannot be used to remove \n from .txt file
+    string_with_characters.tr(",", "").tr(" ", "").tr("\n", "").upcase    # Single quote ("") cannot be used to remove \n from .txt file
   end
   
-  def check_board_type(unprocessed_string)                                                # Check whether use random board or default board or user's input
-    letter_string = remove_characters_from_string(unprocessed_string)
+  def generate_board(letter_string)                                                # Perform check whether use random board or default board or user's input
     
     case letter_string
     when "RANDOM" 
-      return create_random_string()
+      @board = create_random_string()
     when ""
-      return default_board_string()
+      @board = default_board_string()
     else
-      return letter_string
+      letter_string.length == @total_letters ? (@board = letter_string) : (@board = false)
     end
+
   end
 
   def create_random_string()
     alphabets = ('A'..'Z').map(&:to_s) << "*"
-    random_string = (0...@total_letters).map { alphabets[rand(alphabets.length)] }.join
+    (0...@total_letters).map { alphabets[rand(alphabets.length)] }.join
   end
 
   def default_board_string()
     default_board_txt = IO.readlines("../config/default_board.txt")[0]               # Load default board game
     remove_characters_from_string(default_board_txt)
-  end
-
-  def insert_letters_to_board(empty_board, letter_string)
-    empty_board.map.with_index { |box, index| empty_board[index] = letter_string[index] }
-  end
-    
-  def generate_board(letter_string)
-    empty_board = Array.new(@total_letters)
-    board_with_letters = insert_letters_to_board(empty_board, letter_string)
   end
   
   def print_board()
@@ -50,6 +41,7 @@ class Boggle
     end
   end
 
+  # FIXME - Use attr_accessor
   def get_board()
     @board
   end
@@ -64,9 +56,17 @@ class Boggle
 
 end
 
+# Test
+
 board_1 = Boggle.new("A B C,,, D, E F G H, I J K ,,L, s O Pq")
 board_1.print_board()
+puts "\n"
 
 board_3 = Boggle.new("")
 board_3.print_board()
+puts "\n"
+
+board_4 = Boggle.new("RANDOM")
+board_4.print_board()
+puts "\n"
 
